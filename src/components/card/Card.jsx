e9de938a9e106dd5359f407fb6a6dfca04782276
27097 <tapiasgroup@gmail.com>
2024-04-22 10:42:17 -0300

    cargando card

---
+++ src/components/card/Card.jsx
@@ -0,0 +1,21 @@
+import c from './Card.module.css'
+
+
+const Card =()=>{
+    return(
+        <div className={c.card}>
+            
+            <div className={c.up}>
+            </div>
+              
+            
+            <div className={c.down}>
+            </div>
+
+          
+        </div>
+    )
+}
+
+
+export default Card
